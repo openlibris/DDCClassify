37ad35333eecc9ced6b48e15af24a0a30d16556c
mrfakename <me@mrfake.name>
2023-01-17 17:14:36 -0800

    Add caching for 1 year

--- src/DDCClassify/DDC.php
+++ src/DDCClassify/DDC.php
@@ -2,13 +2,31 @@
 namespace fakerybakery/DDCClassify
 class DeweyDecimal
 {
+    private function fgc($url) {
+        if (!is_dir('.ddccache')) {
+            mkdir('.ddccache');
+        }
+        $cache_file = '.ddccache/' . md5($url);
+        if(file_exists($cache_file)) {
+            if(time() - filemtime($cache_file) > 31536000) {
+                $cache = file_get_contents($url);
+                file_put_contents($cache_file, $cache);
+            } else {
+                $cache = file_get_contents($cache_file);
+            }
+        } else {
+            $cache = file_get_contents($url);
+            file_put_contents($cache_file, $cache);
+        }
+        return $cache;
+    }
     public function ClassifyISBN($isbn)
     {
         if (empty($isbn)) {
             return 'Error, empty ISBN!';
         }
         $apione = 'http://classify.oclc.org/classify2/Classify?isbn=' . urlencode($isbn) . '&summary=true';
-        $xml = json_decode(json_encode(simplexml_load_string(file_get_contents($apione))), true);
+        $xml = json_decode(json_encode(simplexml_load_string($this->fgc($apione))), true);
         if (isset($xml['recommendations']['ddc']['mostPopular']['@attributes']['nsfa'])) {
             $ddc = $xml['recommendations']['ddc']['mostPopular']['@attributes']['nsfa'];
         } elseif (isset($xml['recommendations']['ddc']['mostPopular']['nsfa'])) {
@@ -16,7 +34,7 @@ class DeweyDecimal
         } else {
             $wi = $xml["works"]["work"][0]["@attributes"]["wi"];
             $ddcurl = 'http://classify.oclc.org/classify2/Classify?wi=' . $wi . '&summary=true';
-            $ddccontent = json_decode(json_encode(simplexml_load_string(file_get_contents($ddcurl))), true);
+            $ddccontent = json_decode(json_encode(simplexml_load_string($this->fgc($ddcurl))), true);
             if (isset($ddccontent['recommendations']['ddc']['mostPopular'][0]['@attributes']['nsfa'])) {
                 $ddc = $ddccontent['recommendations']['ddc']['mostPopular'][0]['@attributes']['nsfa'];
             } elseif (isset($ddccontent['recommendations']['ddc']['mostPopular']['@attributes']['nsfa'])) {
@@ -33,7 +51,7 @@ class DeweyDecimal
             return 'Error, empty title!';
         }
         $apione = 'http://classify.oclc.org/classify2/Classify?title=' . urlencode($title) . '&author=' . urlencode($author) . '&summary=true';
-        $xml = json_decode(json_encode(simplexml_load_string(file_get_contents($apione))), true);
+        $xml = json_decode(json_encode(simplexml_load_string($this->fgc($apione))), true);
         if (isset($xml['recommendations']['ddc']['mostPopular']['@attributes']['nsfa'])) {
             $ddc = $xml['recommendations']['ddc']['mostPopular']['@attributes']['nsfa'];
         } elseif (isset($xml['recommendations']['ddc']['mostPopular']['nsfa'])) {
@@ -41,7 +59,7 @@ class DeweyDecimal
         } else {
             $wi = $xml["works"]["work"][0]["@attributes"]["wi"];
             $ddcurl = 'http://classify.oclc.org/classify2/Classify?wi=' . $wi . '&summary=true';
-            $ddccontent = json_decode(json_encode(simplexml_load_string(file_get_contents($ddcurl))), true);
+            $ddccontent = json_decode(json_encode(simplexml_load_string($this->fgc($ddcurl))), true);
             if (isset($ddccontent['recommendations']['ddc']['mostPopular'][0]['@attributes']['nsfa'])) {
                 $ddc = $ddccontent['recommendations']['ddc']['mostPopular'][0]['@attributes']['nsfa'];
             } elseif (isset($ddccontent['recommendations']['ddc']['mostPopular']['@attributes']['nsfa'])) {
